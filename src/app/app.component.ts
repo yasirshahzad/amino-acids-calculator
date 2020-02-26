9aadb17490793acc1009c0009d339bec6a34a0b2
Muhammad Yasir <shahzadyasir77@gmail.com>
2020-02-26 12:30:09 +0500

    Final Calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,50 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculator';
+
+  List = [];
+  result: number = 0.0;
+
+  // tslint:disable-next-line: ban-types
+  content: String = '';
+
+  calculate(q) {
+
+    this.List.push(q);
+    this.calculateFormula();
+
+  }
+
+  remove() {
+    this.List.pop();
+    this.calculateFormula();
+  }
+
+  resulta: number = 0.0;
+  myOptions = {
+    'placement': 'center',
+    'show-delay': 500
+  }
+
+  calculateFormula(){
+
+    this.List.forEach(q => {
+      this.resulta = this.resulta + Number.parseFloat(q.value);
+    });
+
+    let waterWeigth:number = 0.0;
+
+
+    if(this.List.length >= 1){
+      waterWeigth = (this.List.length - 1) * 16.0;
+    } else {
+      waterWeigth = 0.0;
+    }
+
+    this.result = this.resulta - waterWeigth ;
+
+    this.resulta = 0.0;
+
+  }
+
 }
